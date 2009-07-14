7136dc6c2f84a8d46ea7459450b269c6b90d753d
Scott Smith <scott@ohlol.net>
2009-07-14 14:33:20 -0700

    Refactored like, everything. Added conf/*.enc (encrypted file) to .gitignore.

--- lib/nachos.rb
+++ lib/nachos.rb
@@ -15,19 +15,20 @@ module Nachos
         :public_key => config_dir + '/public.pem',
         :private_key => config_dir + '/private.pem',
         :secret_key => config_dir + '/secret-key.yml',
-        :secret_data => config_dir + '/secret-stuff.enc'
+        :data_store => config_dir + '/secret-data.enc'
       }
     else
       @@config = {
         :public_key => config[:public_key],
         :private_key => config[:private_key],
         :secret_key => config[:secret_key],
-        :secret_data => config[:secret_data]
+        :data_store => config[:data_store]
       }
     end
   end
 
   class EncryptorException < IOError; end
+  class KeyStoreException < IOError; end
 end
 
 class Nachos::Encryptor
@@ -36,88 +37,158 @@ class Nachos::Encryptor
   # secret_key, secret_iv, data are encrypted and readable by the caller
   # clear_data is writeable/readable by the caller
   attr_reader :secret_key, :secret_iv
-  attr_accessor :clear_data, :data
+  attr_accessor :clear_data, :encrypted_data
+  attr_reader :keystore
 
   # pass an optional encrypted secret key & iv
   # we'll decrypt it and store it for private use
-  def initialize(password, secret_key = nil, secret_iv = nil)
+  def initialize(password, config = {})
+    self.config = config
+    @secret_key = @secret_iv = @clear_data = @encrypted_data = ''
+
     begin
       @public_key = OpenSSL::PKey::RSA.new(File.open(self.config[:public_key]))
       @private_key =
         OpenSSL::PKey::RSA.new(File.open(self.config[:private_key]), password)
       @cipher = OpenSSL::Cipher::Cipher.new('aes-256-cbc')
 
-      if secret_key && secret_iv
-        # @clear_secret_key, @clear_secret_iv are decrypted
-        @clear_secret_key = @private_key.private_decrypt(secret_key)
-        @clear_secret_iv = @private_key.private_decrypt(secret_iv)
-      else
-        @cipher.encrypt
-        @clear_secret_key = @cipher.random_key
-        @clear_secret_iv = @cipher.random_iv
-        @secret_key = @public_key.public_encrypt(@clear_secret_key)
-        @secret_iv = @public_key.public_encrypt(@clear_secret_iv)
-      end
+      @keystore = Nachos::KeyStore.new
+
+      keypair
     rescue Errno::ENOENT
-      raise Nachos::EncryptorException,
-        "Public or private key missing! (maybe both!)"
+      raise Nachos::EncryptorException, "Public or private key missing! " +
+        "(maybe both!)"
+    rescue Nachos::KeyStoreException => e
+      raise
+    rescue => e
+      raise Nachos::EncryptorException, "There was a problem loading or " +
+        "decrypting the keypair and/or keystore! (#{e})" 
     end
   end
 
-  def encrypt
-    @cipher.encrypt
-    @data = @public_key.public_encrypt(@clear_data)
+  def keypair
+    if @keystore.secret_key.empty? || @keystore.secret_iv.empty?
+      begin
+        @clear_secret_key = @cipher.random_key
+        @clear_secret_iv = @cipher.random_iv
+
+        @secret_key = @keystore.secret_key encrypt(@clear_secret_key)
+        @secret_iv = @keystore.secret_iv = encrypt(@clear_secret_iv)
+        @keystore.save_secrets
+      rescue => e
+        raise Nachos::EncryptorException, "There was a problem generating" +
+          "random secret key and/or IV!"
+      end
+    else
+      p "foo"
+      @secret_key = @keystore.secret_key
+      @secret_iv = @keystore.secret_iv
+    end
+
+    @clear_secret_key = decrypt(@secret_key)
+    @clear_secret_iv = decrypt(@secret_iv)
   end
 
-  def decrypt
-    @cipher.decrypt
-    @cipher.key = @clear_secret_key
-    @cipher.iv = @clear_secret_iv
-    @clear_data = @private_key.private_decrypt(@data)
+  def encrypt(str)
+    if str.empty?
+      raise Nachos::EncryptorException, "What do you want to encrypt?"
+    else
+      begin
+      @cipher.encrypt
+      str = @public_key.public_encrypt(str)
+      rescue => e
+        raise Nachos::EncryptorException, "Couldn't encrypt the data! (#{e})"
+      end
+    end
+  end
+
+  def decrypt(str)
+    if str.empty?
+      raise Nachos::EncryptorException, "What do you want to decrypt?"
+    else
+      begin
+        @cipher.decrypt
+         str = @private_key.private_decrypt(str)
+      rescue => e
+        raise Nachos::decryptorException, "Couldn't decrypt the data! (#{e})"
+      end
+    end
+  end
+
+  def load_data
+    begin
+      @encrypted_data = ''
+
+      File.open(self.config[:data_store], 'r') do |f|
+        @encrypted_data = @encrypted_data + f.gets
+      end
+
+      @encrypted_data.chomp!
+      @clear_data = decrypt(@encrypted_data)
+    rescue Errno::ENOENT
+    rescue => e
+      raise Nachos::EncryptorException, "There was a problem reading the " +
+        "data store! (#{e})"
+    end
+  end
+
+  def save_data
+    if @encrypted_data.empty?
+      encrypt
+    end
+
+    begin
+      File.open(self.config[:data_store], 'w') do |f|
+        f.puts @encrypted_data
+      end
+    rescue
+      raise Nachos::EncryptorException, "There was a problem saving the data " +
+        "store!"
+    end
   end
 end
 
 class Nachos::KeyStore
   include Nachos
 
-  attr_accessor :key_e, :iv_e
+  attr_accessor :secret_key, :secret_iv
 
-  def initialize(password, config = {})
-    @keypair_password = password
-    self.config = config
+  def initialize
+    @secret_key = @secret_iv = ''
     get_secrets
   end
 
   def get_secrets
-    unless @key_e && @iv_e
-      begin
-        y_data = YAML::load(File.open(self.config[:secret_key]))
+    begin
+      y_data = YAML::load(File.open(self.config[:secret_key]))
 
-        @key_e = Base64.decode64(y_data[:key])
-        @iv_e = Base64.decode64(y_data[:iv])
-      rescue Errno::ENOENT
-        gen_secrets
-        save_secrets
+      if y_data
+        @secret_key = Base64.decode64(y_data[:key])
+        @secret_iv = Base64.decode64(y_data[:iv])
       end
+      # file was empty for some reason, we'll have to generate a new set.
+      # hope we didn't need to decrypt anything!
+    rescue Errno::ENOENT
+      # fail silently; we'll just generate a new set
+    rescue => e
+      raise Nachos::KeyStoreException, "There was a problem loading the " +
+        "keystore! (#{e})"
     end
-  end
-
-  private
-
-  def gen_secrets
-    encryptor = Nachos::Encryptor.new(@keypair_password)
-    @key_e = encryptor.secret_key
-    @iv_e = encryptor.secret_iv
   end
 
   def save_secrets
     stuff = {
-      :key => Base64.encode64(@key_e),
-      :iv => Base64.encode64(@iv_e)
+      :key => Base64.encode64(@secret_key),
+      :iv => Base64.encode64(@secret_iv)
     }
 
-    File.open(self.config[:secret_key], 'w') do |f|
-      f.puts stuff.to_yaml
+    begin
+      File.open(self.config[:secret_key], 'w') do |f|
+        f.puts stuff.to_yaml
+      end
+    rescue => e
+      raise Nachos::KeyStoreException, "There was a problem saving the " +
+        "keystore! (#{e})"
     end
   end
 end
